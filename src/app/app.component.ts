39c02ea731ab7137da89536a8a85f7dd7a1bb945
TugceOnder <tugceonder775@gmail.com>
2023-11-16 01:08:16 +0300

    pipes dosyası yollandı

---
+++ src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  name = '';
+  date = '';
+  money : number = 0;
+  height: number = 0;
+  miles: number = 0;
+  onNameChange(value : string){
+this.name=value;
+  }
+
+  onDateChange(value : string){
+this.date=value;
+  }
+  onMoneyChange(value:string){
+this.money=parseFloat(value);
+  }
+
+  onHeightChange(value:string){
+this.height =parseFloat(value);
+  }
+  onMilesChange(value:string){
+    this.miles =parseFloat(value);
+  }
+}
